4f14b857b3a7c449b160211005855adaa723880d
Abdumannof <Abdumannof>
2024-04-15 17:04:43 +0300

    header branch yaratildi.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
                 <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
-                
+
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
